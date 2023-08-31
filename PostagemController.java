03c7803a11dbf1895464a98fcdaa89761b4dc4af
Beatriz Doravante <139794497+trizdoravante@users.noreply.github.com>
2023-08-31 00:08:12 -0300

    Add files via upload

--- PostagemController.java
+++ PostagemController.java
@@ -3,6 +3,8 @@ package com.generation.blogpessoal.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +22,16 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
+@GetMapping("/{id}")
+public ResponseEntity<Postagem>getById(@PathVariable Long id){
+	return postagemRepository.findById(id)
+			.map(resposta->ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+}
+@GetMapping("/titulo/{titulo}")
+public ResponseEntity<List<Postagem>>getByTitulo(@PathVariable String titulo){
+	return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+}
+
 
 }
