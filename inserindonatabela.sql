1fc9b1a564039248b309aed076840759bccdf0ad
Beatriz Doravante <139794497+trizdoravante@users.noreply.github.com>
2023-09-12 23:32:52 -0300

    Delete inserindonatabela.sql

--- inserindonatabela.sql
+++
@@ -1,8 +0,0 @@
-USE db_blogpessoal;
-
-INSERT INTO tb_postagens (data, texto, titulo)
-VALUES (current_timestamp(), 'Texto da Postagem 01', 'Postagem 01');
-INSERT INTO tb_postagens (data,texto,titulo)
-VALUES (current_timestamp(), 'Texto da Postagem 02', 'Postagem 02');
-
-SELECT * FROM tb_postagens;
